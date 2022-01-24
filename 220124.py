3ae9e9af3c650f1753a4e74577035dc2ccb0b20f
doyon <doyon3x3@gmail.com>
2022-01-25 00:03:05 +0900

    #1284 compelete

--- 220124.py
+++ 220124.py
@@ -3,28 +3,27 @@ def calculate(n):
   result = 0
   i = 0
   for i in range(len(n)):
-    if n[i] == 0:
+    if n[i] == str(0):
       result += 4
-    elif n[i] == 1:
+    elif n[i] == str(1):
       result += 2
     else:
       result += 3
+
     i += 1
-  space = len(n) + 2
+  space = len(n) + 1
   return (result + space)
 
-print(calculate(str(5611)))
+l = []
 
-# l = []
-
-# while True:
-#   n = input()
-#   if n == str(0):
-#     break
-#   else:
-#     l.append(n)
+while True:
+  n = input()
+  if n == str(0):
+    break
+  else:
+    l.append(n)
 
 
-# x = list(map(calculate,l))
-# for j in x:
-#   print(j)
+x = list(map(calculate,l))
+for j in x:
+  print(j)
