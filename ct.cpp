e72ff6a17e8d4770c2c90e3fa634133f0d72eae1
doyeon <88149429+doyon33@users.noreply.github.com>
2023-03-09 23:25:50 +0900

    #1439
    counts for making numbers all the same

---
+++ ct.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//1439
+int main() {
+    string s;
+    int count = 0;
+
+    cin >> s;
+    for (string::iterator it = s.begin() + 1; it < s.end(); ++it) {
+        if (*it != *(it - 1)) {
+            ++count;
+        }
+    }
+    if (count % 2 == 0) {
+        cout << count / 2;
+    }
+    else
+        cout << (count+1) / 2; // 2;
+    
+}
